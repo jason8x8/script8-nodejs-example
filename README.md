10f3401ae3bd7049214ba03e643f8327303e4292
Jason Liu <jason.liu@8x8.com>
2018-02-19 15:23:21 -0800

    Create a simple example to start hello world

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# script8-nodejs-example
+# Script8 Node.js Quick Start Guide
+
+This guide will walk you through deploying a Node.js application on [Deis Workflow][].
+
+## Dowload ngrok
+
+go to https://ngrok.com/download to download ngrok
+
+## Usage
+
+```console
+$ git clone https://github.com/jason8x8/script8-nodejs-example.git
+$ cd script8-nodejs-example
+$ npm install --save 
+$ node server.js
+$ ./ngrok http 5000
+
+Copy the https URL shown on ngrok, configure Script8 developer account set it as voiceWebHookHttpURL, and http method GET or POST
+```
+
+## Additional Resources
+
---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var fs = require('fs');
+var exec = require('child_process').exec;
+var app = express();
+var path = require('path');
+var basicAuth = require('basic-auth-connect');
+
+app.use(basicAuth('admin', 'password'));
+
+app.use('/music', express.static("./music"));
+app.use('/xml', express.static("./xml"));
+
+// app router
+app.post('/', main);
+app.get('/', main);
+
+app.post('/logger', logger);
+
+function main(req, res) {
+    console.log(req.query);
+    console.log(req.body);
+
+    console.log("Here's an hello world example");
+
+    fs.readFile(path.join(__dirname,'./xml/cpxml-helloworld.xml'), 'utf8', function(err, data) {
+        if (err) {
+            res.writeHead(404, {
+                'Content-Type': 'application/xml'
+            });
+            res.end(data);
+            return console.log(err);
+        }
+        console.log(data);
+        res.writeHead(200, {
+            'Content-Type': 'application/xml'
+        });
+        res.end(data);
+    });
+}
+
+function logger(req, res) {
+    //console.log(req.body);
+
+    var level = req.body.level;
+    var log = req.body.log;
+    var ts = req.body.ts;
+    var d = new Date(Number(ts));
+
+    console.log(d.toLocaleTimeString() + " " + level + " : " + log);
+    res.writeHead(200);
+    res.end("OK");
+}
+
+/* Use PORT environment variable if it exists */
+var port = process.env.PORT || 5000;
+server = app.listen(port, function () {
+  console.log('Server listening on port %d in %s mode', server.address().port, app.settings.env);
+});
